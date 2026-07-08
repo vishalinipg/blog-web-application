468f1fede6dc6406437200fb3f5e3a4a8ab1c9d8
vishalinipg <vishalinipg@gmail.com>
2026-07-08 10:27:27 +0530

    refactor: rearrange imports in apps.py

--- blog/apps.py
+++ blog/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
 
 class BlogConfig(AppConfig):
@@ -6,7 +7,8 @@ class BlogConfig(AppConfig):
     name = "blog"
 
     def ready(self):
-        from django.db.models.signals import post_migrate
+        # Local import here is strictly required to prevent AppRegistryNotReady
+        # circular loading exception when django.contrib.auth models are loaded.
         from .signals import create_groups_and_permissions
 
         post_migrate.connect(create_groups_and_permissions, sender=self)
