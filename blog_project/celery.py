cbb2f01007c40891510226d4b36c3cbf48c9980b
vishalinipg <vishalinipg@gmail.com>
2026-07-09 15:14:03 +0530

    clean: remove unused celery debug_task

--- blog_project/celery.py
+++ blog_project/celery.py
@@ -1,4 +1,5 @@
 import os
+
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
@@ -14,8 +15,3 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
-
-
-@app.task(bind=True, ignore_result=True)
-def debug_task(self):
-    print(f"Request: {self.request!r}")
